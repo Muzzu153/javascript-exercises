e3d073156ba87ecdd0831e5814870edf9a012021
Muzaffer <muzzusyed153@gmail.com>
2023-01-12 19:25:16 +0530

    Create the function that reverse the passed string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+let random = Math.floor(Math.random()*1000)
 
+const repeatString = function(word, num) {
+    if (num<0) return "ERROR";
+    let string = "";
+    for(let i=0; i<num; i++){
+        string += word;
+    }
+    return string;
 };
+
+repeatString("hey", 3)
+repeatString("hey", 10)
+repeatString("hey", 1)
+repeatString("hey", )
+repeatString("hey", -1)
+repeatString("hey", )
+repeatString(" ", random)
 
 // Do not edit below this line
 module.exports = repeatString;
