81f2477fd4df152a27f2a117b3aa5c0c7d9f3d5e
Muzaffer <muzzusyed153@gmail.com>
2023-01-21 19:47:17 +0530

    Create the function to remove the given numbers

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function (...args) {
+    let array = args[0];
+    let numbers = [];
 
-};
+    array.forEach((element) => {
+        if (!args.includes(element)) {
+            numbers.push(element)
+        }
+    });
+    return numbers;
+}
 
+
+removeFromArray([1, 2, 3, 4], 3);
+removeFromArray([1, 2, 3, 4], 3,2);
+removeFromArray([1, 2, 3, 4], 7,"tacos");
+removeFromArray([1, 2, 3, 4], 7,2);
+removeFromArray([1, 2, 3, 4], 1,2,3,4);
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
+removeFromArray([1, 2, 3], "1", 3);
 // Do not edit below this line
 module.exports = removeFromArray;
