92f397d24f49c79af40ae4554faa358b445185fe
Muzaffer <muzzusyed153@gmail.com>
2023-01-21 18:30:43 +0530

    Create function that reverses the given string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reverse = word.split("").reverse().join("");
+    return reverse;
 };
+
+reverseString("hello");
+reverseString("hello there");
+reverseString("123! abc!");
+reverseString("");
 
 // Do not edit below this line
 module.exports = reverseString;
