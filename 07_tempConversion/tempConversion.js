368bc832a4cce80a020dd395bf8db99fb923183b
Muzaffer <muzzusyed153@gmail.com>
2023-01-22 16:47:38 +0530

    Create a function for temperature conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (celsius) {
+  let convertTemp = Math.round((celsius - 32) * (5 / 9) * 10) / 10;
+  return convertTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (fahrenheit) {
+  let convertTemp = Math.round(((fahrenheit * 9 / 5) + 32) * 10) / 10;
+  return convertTemp;
 };
 
+convertToCelsius(32);
+convertToCelsius(100);
+convertToCelsius(-100);
+
+convertToFahrenheit(0);
+convertToFahrenheit(73.2);
+convertToFahrenheit(-10);
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
