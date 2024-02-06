0948708f811e6f415faebb86ef873e3876d43720
Muzaffer <muzzusyed153@gmail.com>
2024-02-06 12:36:50 +0530

    Finish the palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (sentence) {
+  const processedstring = sentence.toLowerCase().replace(/[^a-z0-9]/g, "");
+  let reversedString = processedstring.split("").reverse().join("");
+  return reversedString === processedstring;
 };
 
 // Do not edit below this line
