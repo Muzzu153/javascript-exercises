6177d887d9cb49d0ad35aa7ad9c846704634fe7a
Muzaffer <muzzusyed153@gmail.com>
2024-02-06 12:32:01 +0530

    Finish all tests in calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return (num1 += num2);
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+// const sum = function (arr) {
+//   let sum1 = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum1 += arr[i];
+//   }
+//   return sum1;
+// };
+
+const sum = function (arr) {
+  // let sum1 = 0;
+  return arr.reduce((sum, el) => sum += el, 0)
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((sum, el) => sum *= el, 1)
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if(num===0) return 1;
+  let numbersss = 1;
+  for (let i = num; i > 0; i--) {
+      numbersss *= i;
+  }
+  return numbersss;
 };
 
 // Do not edit below this line
@@ -29,5 +43,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
